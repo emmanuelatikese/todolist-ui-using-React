16d7ebc480f02fd7aaff5e570c08addc1ca68da0
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-12-10 14:09:22 -0800

    add

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import Signal from './Mobile Signal.png'
 import Wifi from './Wifi.png'
+import Alarm from './Alarm.png'
+import Bluetooth from "./Bluetooth.png"
+import Battery from "./Battery.png"
+import Snowman from "./Snowman.png"
 
 
 export default function NavBar(){
@@ -10,6 +14,17 @@ export default function NavBar(){
         <img src={Signal} alt='' id='signal-img'/>
         <p id='figma-word'>Figma</p>
         <img src={Wifi} alt="" id="wifi-img"/>
+        <p id='time-word'>9:41 AM</p>
+        <img src={Alarm} alt="" id="alarm-img"/>
+        <img src={Bluetooth} alt="" id='bluetooth-img'/>
+        <p id='battery-percentage'>100%</p>
+        <img src={Battery} alt="" id='battery-img'/>
+      </div>
+      <div className='picture-content'>
+            <img src={Snowman} alt="" id='snowman-img'/>
+      </div>
+      <div className='words-content'>
+        <p id='main-topic'>Main items</p>
       </div>
     </nav>
     )
