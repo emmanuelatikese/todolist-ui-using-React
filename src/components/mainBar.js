fe66ceeac89e8784e597b026374b72fcab33f62c
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-12-12 07:31:38 -0800

    add

--- src/components/mainBar.js
+++ src/components/mainBar.js
@@ -8,7 +8,27 @@ export default function MainBar(){
             <div class='list-container'>
                 <div className='list-content'>
                     <input type="checkbox" class='checkbox'/>
-                    <p class='list-word'>listname</p>
+                    <p class='list-word' id='check-id'>Check emails</p>
+                    <img src={Image1} alt="" id='delete-img'/>
+                </div>
+                <div className='list-content'>
+                    <input type="checkbox" class='checkbox'/>
+                    <p class='list-word' id='git-id'>Update Github</p>
+                    <img src={Image1} alt="" id='delete-img'/>
+                </div>
+                <div className='list-content'>
+                    <input type="checkbox" class='checkbox'/>
+                    <p class='list-word' id='doc-id'>Submit documentation</p>
+                    <img src={Image1} alt="" id='delete-img'/>
+                </div>
+                <div className='list-content'>
+                    <input type="checkbox" class='checkbox'/>
+                    <p class='list-word' id='study-id'>Study</p>
+                    <img src={Image1} alt="" id='delete-img'/>
+                </div>
+                <div className='list-content'>
+                    <input type="checkbox" class='checkbox'/>
+                    <p class='list-word' id='shop-id'>Shopping</p>
                     <img src={Image1} alt="" id='delete-img'/>
                 </div>
             </div>
