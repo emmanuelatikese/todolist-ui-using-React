4fe3c6c179562fcf3aa07e6bbb84bc127d6e51ee
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-12-11 17:38:19 -0800

    made some changes and to save the work

--- src/components/mainBar.js
+++ src/components/mainBar.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Image1 from "./Layer 6.png"
 
 export default function MainBar(){
 
     return (
         <main>
-            
+            <div class='list-container'>
+                <div className='list-content'>
+                    <input type="checkbox" class='checkbox'/>
+                    <p class='list-word'>listname</p>
+                    <img src={Image1} alt="" id='delete-img'/>
+                </div>
+            </div>
         </main>
     )
 }
